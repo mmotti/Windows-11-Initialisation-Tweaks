a7a17eda9d238f9dd5e03a558508d4151ee0c9d6
mmotti <matthew.w.motti@gmail.com>
2025-03-18 00:25:53 +0000

    Fix some issues where the working directory isn't captured properly.

--- Run.bat
+++ Run.bat
@@ -1,11 +1,15 @@
 @echo off
+
+:: Capture the directory of the script
+set "scriptDir=%~dp0"
+
 :: Check if Windows Terminal (wt.exe) is available.
 where wt >nul 2>nul
 if %errorlevel%==0 (
     :: Run PowerShell script in Windows Terminal.
     :: Script will prompt for elevation if required.
-    wt new-tab -p "PowerShell" powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0Tweaks.ps1"
+    wt new-tab -p "PowerShell" powershell -NoProfile -ExecutionPolicy Bypass -File "%scriptDir%Tweaks.ps1"
 ) else (
     :: Run PowerShell script normally with elevated privileges.
-    powershell -NoProfile -ExecutionPolicy Bypass -Command "& { Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0Tweaks.ps1\"' -Verb RunAs }"
+    PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%scriptDir%Tweaks.ps1""' -Verb RunAs}"
 )
